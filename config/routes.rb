f487da70f97a05ea3f1c3e05079cb98b994d116c
Rouan Thompson <rouan.thompson@gmail.com>
2020-07-18 23:19:30 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #get http req for /students that maps to StudentsController action index
+  get '/students', to: 'students#index'
+  #resources :students, only: :index
+
 end
